7cf447347a85f4c0ac757a9b70e9f3421629cd61
lillyand <148947879+lillyand@users.noreply.github.com>
2026-04-22 13:55:06 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,S=0,szamj,szam,pref=0;
+    cout<<"n=";
+    cin>>n;
+    while(n>0){
+        szamj=szam%10;
+        szam=szam/10;
+        pref=pref+szamj;
+        pref=pref*10;
+        S=S+pref;
+    }
+    cout<<S;
+    return 0;
+}
