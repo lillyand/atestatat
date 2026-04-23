b47e79ee81892cd181dc7e68f63332b0835e6727
lillyand <148947879+lillyand@users.noreply.github.com>
2026-04-23 07:09:35 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,S=0;
+    cout<<"n=";
+    cin>>n;
+    while(n>0){
+        S=S+n;
+        n=n/10;
+    }
+    cout<<S;
+    return 0;
+}
